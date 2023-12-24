e15ab7931f98c599803997423e547f0b6793d442
snzolnikov <snzolnikov@gmail.com>
2023-12-24 13:06:43 +0500

    вариант кода c result

--- multithreading/04_thred_pool_submit.py
+++ multithreading/04_thred_pool_submit.py
@@ -5,31 +5,37 @@ import time
 def div(divisor, limit):
     print(f'started div= {divisor}')
 
+    result = 0
     for x in range(1, limit):
         if x % divisor == 0:
-            print(f'divisor={divisor}, x={x}')
+            result += x
+            # print(f'divisor={divisor}, x={x}')
         time.sleep(0.2)
     print(f'\nended div={divisor}', end='\n')
+    return result
 
 
 if __name__ == '__main__':
     print('started main')
 
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-    #     executor.submit(div, 3, 25)
-    #     executor.submit(div, 5, 25)
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        futures.append(executor.submit(div, 3, 25))
+        futures.append(executor.submit(div, 5, 25))
+
+        while futures[0].running() and futures[1].running():
+            print('.', end='')
+            time.sleep(1)
+
+        for f in futures:
+            print(f'{f.result()}')
+
+    print('After with block')
+
+    # executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    # executor.submit(div, 3, 25)
+    # executor.submit(div, 5, 25)
     #
-    #     print('Immedietly printed out after submit')
+    # executor.shutdown(wait=True)
     #
-    # print('After with block')
-
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-    executor.submit(div, 3, 25)
-    executor.submit(div, 5, 25)
-
-    executor.shutdown(wait=True)
-
     print('\nmain ended')
-
-
-
