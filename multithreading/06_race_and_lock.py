07621c4ad576401aade575dd84087fe515d9dd1a
snzolnikov <snzolnikov@gmail.com>
2024-01-02 12:05:33 +0500

    Работает корректно

--- multithreading/06_race_and_lock.py
+++ multithreading/06_race_and_lock.py
@@ -1,22 +1,35 @@
 import concurrent.futures.thread
+import threading
 import time
 
 
 class BankAccount:
     def __init__(self):
         self.balance = 100 # shared data / resource
+        self.lock_obj = threading.Lock()
 
     def update(self, transaction, amount):
         print(f'{transaction} started')
-        tmp_amount = self.balance
-        tmp_amount += amount
-        time.sleep(1)
-        self.balance = tmp_amount
 
+        with self.lock_obj:
+            tmp_amount = self.balance
+            tmp_amount += amount
+            time.sleep(1)
+            self.balance = tmp_amount
         print(f'{transaction} ended')
 
 
 if __name__ == '__main__':
+    # lock_obj = threading.Lock()
+    # print(lock_obj.locked())
+    #
+    # lock_obj.acquire()
+    # print(lock_obj.locked())
+    #
+    # lock_obj.release()
+    # print(lock_obj.locked())
+
+
     acc = BankAccount()
     print(f'Main started. acc.balance={acc.balance}')
 
